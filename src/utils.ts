07be42d954837f1d10b08dc20ae824c005388fc3
Devil7372 <devilgamingofficial59@gmail.com>
2025-11-18 21:30:03 +0530

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,14 @@
+export function normalizeTag(tag: string) {
+  tag = tag.trim().toUpperCase();
+  if (!tag.startsWith("#")) tag = "#" + tag;
+  return tag;
+}
 
+export function shortPlayerSummary(player: any) {
+  return `
+👤 नाम: ${player.name}
+🏰 Town Hall: ${player.townHallLevel}
+⭐ Level: ${player.expLevel}
+🏆 Trophies: ${player.trophies}
+`.trim();
+}
